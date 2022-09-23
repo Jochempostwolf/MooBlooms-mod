314fdc5d4330bd56bcf8589d61c2bf6e818f42cb
Jochem <jochempostwolf@gmail.com>
2022-09-23 21:56:57 +0200

    Initial commit

---
+++ src/main/java/net/jochem/moobloom/MooBloom.java
@@ -0,0 +1,14 @@
+package net.jochem.moobloom;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MooBloom implements ModInitializer {
+	public static final String MOD_ID = "moobloom";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/jochem/moobloom/MooBloomClient.java
@@ -0,0 +1,10 @@
+package net.jochem.moobloom;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MooBloomClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
